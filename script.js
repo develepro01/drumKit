1da89df2fbdfbf85bdd900eac41430c5f4dc71a6
develepro01 <hargun.chawla10@gmail.com>
2023-08-23 16:46:43 +0200

    added animation and made case insensitive

--- script.js
+++ script.js
@@ -1,75 +1,71 @@
-
 //enthält Element mit der class "key"
 
-var keyElements = document.querySelectorAll(".key")
+console.log("test");
 
-
-
+var keyElements = document.querySelectorAll(".key");
 var buttonArray = [];
 var idArray = [];
 
 // buttonArray enthält alle verfügbaren Buchstaben
 //idArray enthält alle id der Buchstaben
 
-keyElements.forEach(function(element) {
-    buttonArray.push(document.getElementById(element.id).children[0].textContent);
-    idArray.push(element.id);
+keyElements.forEach(function (element) {
+  buttonArray.push(document.getElementById(element.id).children[0].textContent);
+  idArray.push(element.id);
 });
 
-console.log(buttonArray);
-console.log(idArray);
+// über diesen Index wird aus dem idArray die id des Buchstaben ermmittelt
+//diese wird mit dem String "Audio" verbunden, um aid die richige Audiodatei zuzugreifen
 
+document.addEventListener("keypress", function (event) {
+  // es wird überprüft ob der gedrückte Buchstabe im buttonArray vorhanden ist
+  if (buttonArray.includes(event.key.toUpperCase())) {
+    //indexElement ist der index des Buchstaben im buttonArray
 
+    var indexElement = buttonArray.indexOf(event.key.toUpperCase());
 
+    console.log(idArray[indexElement] + "Audio");
 
-
-document.addEventListener("keypress", function(event) {
-    
-// es wird überprüft ob der gedrückte Buchstabe im buttonArray vorhanden ist
-    if(buttonArray.includes(event.key)){
-
-        //indexElement ist der index des Buchstaben im buttonArray
-
-        let indexElement = buttonArray.indexOf(event.key)
-       
-
-        console.log(idArray[indexElement] + "Audio");
-
-        // über diesen Index wird aus dem idArray die id des Buchstaben ermmittelt
-        //diese wird mit dem String "Audio" verbunden, um aid die richige Audiodatei zuzugreifen
-        var audio = new Audio(document.getElementById(idArray[indexElement] + "Audio").src);
-        audio.play()
-
-
-        //Animation für Knopfdruck
-
-        document.getElementById(idArray[indexElement]).style.height = "35px";
-        document.getElementById(idArray[indexElement]).style.width = "35px";
-
-
-        setTimeout(pressAnimationReturn, 200)
-
-        function pressAnimationReturn(){
-
-        document.getElementById(idArray[indexElement]).style.height = "32px";
-        document.getElementById(idArray[indexElement]).style.width = "32px";
-
-        }
-
-        
-
-
+    function playAudio() {
+      var audio = new Audio(
+        document.getElementById(idArray[indexElement] + "Audio").src
+      );
+      audio.play();
     }
 
+    playAudio();
+
+    //Animation für Knopfdruck
+    document.getElementById(idArray[indexElement]).classList.add("playing");
+
+    setTimeout(pressAnimationReturn, 200);
+
+    function pressAnimationReturn() {
+      document
+        .getElementById(idArray[indexElement])
+        .classList.remove("playing");
+    }
+
+    if (idArray[indexElement] == "74" || idArray[indexElement] == "75") {
+      document.getElementById("hihat-top").style.top = "170px";
+      setTimeout(hihatReturn, 200);
+      function hihatReturn() {
+        document.getElementById("hihat-top").style.top = "162px";
+      }
+
+      playAudio();
+    }
+
+    if (idArray[indexElement] == "72" || idArray[indexElement] == "73") {
+      document.getElementById("crash-ride").style.transform =
+        "rotate(-20deg) scale(1.5)";
+      setTimeout(crashReturn, 200);
+      function crashReturn() {
+        document.getElementById("crash-ride").style.transform =
+          "rotate(-7.2deg) scale(1.5)";
+      }
+
+      playAudio();
+    }
+  }
 });
-
-
-
-
-
-
-
-
-
-
-    
